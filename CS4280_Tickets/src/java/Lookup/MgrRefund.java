8b494c0c8623c9f858197f96e18cb81687d61c8a
xingguo3 <xingguohk@gmail.com>
2017-04-19 16:20:46 +0800

    0419

--- CS4280_Tickets/src/java/Lookup/MgrRefund.java
+++ CS4280_Tickets/src/java/Lookup/MgrRefund.java
@@ -7,6 +7,7 @@ package Lookup;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -28,6 +29,33 @@ public class MgrRefund {
            
            String strQl = null;
            strQl = "DELETE FROM dbo.history WHERE id= '"+id+"'";
+           stmt.execute(strQl);
+           // add thic action into another database
+           // action: add, fid:
+        }finally{
+            if (stmt!=null) {
+                stmt.close();
+            } 
+           try{
+              if(con!=null)
+                 con.close();
+           }catch(SQLException se){
+              se.printStackTrace();
+           }
+        }
+    }
+    public static void refuse(String id) throws ClassNotFoundException, SQLException{
+        Connection con = null;
+        Statement stmt = null;
+        try{
+
+           Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+           con = DriverManager.getConnection("jdbc:sqlserver://w2ksa.cs.cityu.edu.hk:1433;databaseName=aiad092_db", "aiad092", "aiad092");
+           
+           String strQl = null;
+           strQl = "UPDATE FROM dbo.history SET status=? WHERE id= '"+id+"'";
+           PreparedStatement ps = con.prepareStatement(strQl);
+           ps.setInt(1, 1);
            stmt.execute(strQl);
            // add thic action into another database
            // action: add, fid:
